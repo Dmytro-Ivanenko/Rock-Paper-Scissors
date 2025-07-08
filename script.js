7303ede6850bc90fb04bb151d4371cfd0a6abd6a
Dmytro Ivanenko <107716921+Dmytro-Ivanenko@users.noreply.github.com>
2025-07-08 20:42:42 +0100

    add images, add font

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. Round counter
+//  2  Score screen
+//  3  Timer screen
+//  4. Computer choice screen
+//  5. Player buttons (3)
+let round = 1;
+let comp_score = 0;
+let player_score = 0;
+let timer = 3;
