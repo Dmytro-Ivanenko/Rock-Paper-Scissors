400a600326e080f2b23bcec27507998472c1b3d9
Dmytro Ivanenko <107716921+Dmytro-Ivanenko@users.noreply.github.com>
2025-07-31 20:49:07 +0100

    add all game functions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //  5. Player buttons (3)
 // We are playing only 5 rounds!
 
-let round = 1;
+let round = 0;
 let comp_score = 0;
 let player_score = 0;
 let compHand = "";
@@ -12,9 +12,9 @@ const handArr = ["rock", "paper", "scissors"];
 
 //
 
-const btnRock = document.querySelector("#p_rock");
-const btnPaper = document.querySelector("#p_paper");
-const btnScissors = document.querySelector("#p_scissors");
+const btnRock = document.querySelector("#button_rock");
+const btnPaper = document.querySelector("#button_paper");
+const btnScissors = document.querySelector("#button_scissors");
 
 const roundElem = document.querySelector("#round");
 const computerScoreElem = document.querySelector("#computer_score");
@@ -22,6 +22,7 @@ const playerScoreElem = document.querySelector("#player_score");
 
 // Functions
 function changeRound() {
+      round++;
       roundElem.textContent = `Round ${round}`;
 }
 
@@ -29,9 +30,13 @@ function addScore(winner) {
       if (winner == "computer") {
             comp_score++;
             computerScoreElem.textContent = `Computer: ${comp_score}`;
+
+            alert("Computer + 1");
       } else if (winner == "player") {
             player_score++;
             playerScoreElem.textContent = `Player: ${player_score}`;
+
+            alert("Player + 1");
       } else {
             alert("It is a draw!");
       }
@@ -41,9 +46,11 @@ function getMathRandom(max) {
       return Math.floor(Math.random() * max);
 }
 
-function whoIsWinner() {
-      compHand = handArr[getMathRandom(3)];
+function getNewCompHand() {
+      return handArr[getMathRandom(3)];
+}
 
+function whoIsWinner() {
       if (playerHand == compHand) {
             return null;
       }
@@ -57,15 +64,71 @@ function whoIsWinner() {
                   return "computer";
             }
             return "player";
+      } else if (playerHand == "paper") {
+            if (compHand == "scissors") {
+                  return "computer";
+            }
+            return "player";
       }
 }
 
-// add image logic
+function reset() {
+      comp_score = 0;
+      player_score = 0;
+      round = 0;
 
-function changeImg() {}
-
-function playGame() {}
-
-for (let round = 0; round <= 5; round++) {
-      playGame();
+      roundElem.textContent = `Round ${round}`;
+      playerScoreElem.textContent = `Player: ${player_score}`;
+      computerScoreElem.textContent = `Computer: ${comp_score}`;
 }
+
+function changeImg(changeType) {
+      if (changeType == "newGame") {
+            let avatars = document.querySelectorAll("#p_avatar, #com_avatar");
+
+            avatars.forEach((e) => {
+                  e.classList.toggle("hidden");
+            });
+      } else if (changeType == "hands") {
+            let hands = document.querySelectorAll(`#com_${compHand}, #p_${playerHand}`);
+            hands.forEach((e) => {
+                  e.classList.toggle("hidden");
+            });
+      }
+}
+
+function updateHands() {
+      changeImg("newGame");
+      playerHand = hand;
+      compHand = getNewCompHand();
+      changeImg("hands");
+}
+
+function playGame(hand) {
+      changeRound();
+
+      if (round == 1) {
+            updateHands();
+            addScore(whoIsWinner());
+      } else if (round <= 5) {
+            updateHands();
+
+            addScore(whoIsWinner());
+      } else {
+            alert("New Game!");
+
+            changeImg("hands"); // hide last hands
+            changeImg("newGame"); // show avatars
+            reset();
+      }
+}
+
+btnRock.addEventListener("click", () => {
+      playGame("rock");
+});
+btnPaper.addEventListener("click", () => {
+      playGame("paper");
+});
+btnScissors.addEventListener("click", () => {
+      playGame("scissors");
+});
